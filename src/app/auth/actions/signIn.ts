65270af14d08f5e197c9480dc20ae32c663f7637
Dahmane13 <dahmaneabdelmalek0@gmail.com>
2024-02-25 13:19:01 +0100

    adding auth

---
+++ src/app/auth/actions/signIn.ts
@@ -0,0 +1,21 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export const signIn = async (formData: FormData) => {
+    "use server";
+
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const supabase = createClient();
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      return redirect("/auth?error=Could not authenticate user");
+    }
+
+    return redirect("/");
+  };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={
+          inter.className + "w-screen h-screen flex justify-center items-center"
+        }
+      >
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
